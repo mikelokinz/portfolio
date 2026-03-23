bb0966fa83640da97adc0068f36f7b51d2d6873a
mikelokinz <mikelokinz@gmail.com>
2026-03-23 10:31:39 +0530

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
+console.log(
+  "%c🕵️‍♂️ Ah! A fellow developer. \n%cSince you're poking around in my console, you should probably just hire me. Feel free to reach out via my Contact page!",
+  "color: #00ff00; font-size: 20px; font-weight: bold;",
+  "color: white; font-size: 14px;"
+);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
