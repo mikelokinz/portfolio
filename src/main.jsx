05c8895515c29861dee8a0f32257ac6297efffa3
mikelokinz <mikelokinz@gmail.com>
2026-03-23 10:36:50 +0530

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx';
 import './index.css';
 
 console.log(
-  "%c🕵️‍♂️ Ah! A fellow developer. \n%cSince you're poking around in my console, you should probably just hire me. Feel free to reach out via my Contact page!",
+  "%c🕵️‍♂️ Yooo...Wassup✌🏿. \n%cSince you're poking around in my console, you should probably just hire me. Feel free to reach out via my Contact page!",
   "color: #00ff00; font-size: 20px; font-weight: bold;",
   "color: white; font-size: 14px;"
 );
